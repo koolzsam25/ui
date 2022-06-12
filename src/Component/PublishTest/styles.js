57d58ed3ef45a5d1b68c0eaa1a2ace76954bbb88
mahalaxmiSK <mahalaxmi.kumbari@gmail.com>
2022-06-12 14:42:54 +0530

    Add test button style on focus

--- src/Component/PublishTest/styles.js
+++ src/Component/PublishTest/styles.js
@@ -7,6 +7,11 @@ export const CommonButton = styled(Button)`
         background-color: ${props => props.theme.secondary};
         border-color: ${props => props.theme.secondary};
     }
+    &:focus {
+        background-color: ${props => props.theme.active};
+        border-color: ${props => props.theme.active};
+        box-shadow: 0 0 0 0.25rem rgb(147 166 184 / 50%);
+    }
     background-color: ${props => props.theme.primary};
     
     margin-left: 15px
